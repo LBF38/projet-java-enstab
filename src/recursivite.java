5f1ae88fc5ff1813f059e381ed955a68b8ee8f88
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-10 11:33:58 +0200

    implémentation factorielle

--- src/recursivite.java
+++ src/recursivite.java
@@ -1,3 +1,14 @@
 public class recursivite {
-    
+    public static int factorielle(int nombre) {
+        if (nombre==1){
+            return 1;
+        }
+        return factorielle(nombre-1)*nombre;
+    }
+    public static void main(String args[]) {
+        int nombre=5;
+        System.out.println(factorielle(nombre));
+
+    }
 }
+
