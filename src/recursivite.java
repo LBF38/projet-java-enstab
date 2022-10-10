faab40df54768879e48b354b387385d756ba92af
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-10 12:04:04 +0200

    implémentation fibonacci

--- src/recursivite.java
+++ src/recursivite.java
@@ -1,14 +1,25 @@
 public class recursivite {
     public static int factorielle(int nombre) {
-        if (nombre==1){
+        if (nombre==1 || nombre==0){
             return 1;
         }
         return factorielle(nombre-1)*nombre;
     }
+    public static int fibonacci(int n) {
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
     public static void main(String args[]) {
+        System.out.println("Factoriel de 5: doit être égal à 120");
         int nombre=5;
         System.out.println(factorielle(nombre));
-
+        System.out.println("Fibonacci de 16: doit être égal à 987");
+        System.out.println(fibonacci(16));
     }
 }
 
