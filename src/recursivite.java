b4663f73d557947b421407e403192e65ba34d2d6
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-17 10:26:03 +0200

    Reformatage du code

--- src/recursivite.java
+++ src/recursivite.java
@@ -1,37 +1,38 @@
 public class recursivite {
     public static int factorielle(int nombre) {
-        if (nombre==1 || nombre==0){
+        if (nombre == 1 || nombre == 0) {
             return 1;
         }
-        return factorielle(nombre-1)*nombre;
+        return factorielle(nombre - 1) * nombre;
     }
+
     public static int fibonacci(int n) {
-        if (n==0){
+        if (n == 0) {
             return 0;
         }
-        if (n==1){
+        if (n == 1) {
             return 1;
         }
-        return fibonacci(n-1)+fibonacci(n-2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
     public static int ackermann(int m, int n) {
-        if (m==0){
-            return n+1;
+        if (m == 0) {
+            return n + 1;
         }
-        if (n==0){
-            return ackermann(m-1,1);
+        if (n == 0) {
+            return ackermann(m - 1, 1);
         }
-        return ackermann(m-1, ackermann(m,n-1));
+        return ackermann(m - 1, ackermann(m, n - 1));
     }
-    
+
     public static void main(String args[]) {
         System.out.println("Factoriel de 5: doit être égal à 120");
-        int nombre=5;
+        int nombre = 5;
         System.out.println(factorielle(nombre));
         System.out.println("Fibonacci de 16: doit être égal à 987");
         System.out.println(fibonacci(16));
         System.out.println("Ackermann de 2 et 3: doit être égal à 9");
-        System.out.println(ackermann(2,3));
+        System.out.println(ackermann(2, 3));
     }
 }
-
