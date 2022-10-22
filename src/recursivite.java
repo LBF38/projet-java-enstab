b9af59dbc0b12d6ae1b183af71a7d207f0f74f4d
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-10-22 17:59:12 +0200

    Implémentation de la q4 : calcul du PGCD

--- src/recursivite.java
+++ src/recursivite.java
@@ -26,13 +26,23 @@ public class recursivite {
         return ackermann(m - 1, ackermann(m, n - 1));
     }
 
+    public static int pgcd(int a,int b) {
+        if (b==0){
+            return a;
+        }
+        int c=a%b;
+        return pgcd(b, c);
+    }
+
     public static void main(String args[]) {
         System.out.println("Factoriel de 5: doit être égal à 120");
         int nombre = 5;
         System.out.println(factorielle(nombre));
         System.out.println("Fibonacci de 16: doit être égal à 987");
         System.out.println(fibonacci(16));
-        System.out.println("Ackermann de 2 et 3: doit être égal à 9");
-        System.out.println(ackermann(2, 3));
+        System.out.println("Ackermann de 2 et 2: doit être égal à 7");
+        System.out.println(ackermann(2, 2));
+        System.out.println("PGCD de 79 et 35: doit être égal à 1");
+        System.out.println(pgcd(79, 35));
     }
 }
