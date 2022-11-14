61f01f66e880b5483dfde10915016d0229b258f8
LBF38 <contact.lbf38@gmail.com>
2022-11-14 09:51:10 +0100

    Implemented Bibliotheque and modified TestDocuments
    Ajout des différentes méthodes de Bibliothèques.

--- src/bibliotheque/TestDocuments.java
+++ src/bibliotheque/TestDocuments.java
@@ -2,6 +2,7 @@ package bibliotheque;
 
 public class TestDocuments {
     public static void main(String[] args) {
+        System.out.println("Testing Documents");
         Novel leRougeEtLeNoir = new Novel(1, "Le rouge et le noir", "Stendhal", 563, null);
         Novel leursEnfantsApresEux = new Novel(2, "Leurs enfants après eux", "Nicolas Matthieu", 354,
                 Novel.LiteraryPrice.GONCOURT);
@@ -14,5 +15,17 @@ public class TestDocuments {
         System.out.println("histoire6eme=" + histoire6eme.toString());
         System.out.println("geoJuin2019=" + geoJuin2019.toString());
         System.out.println("harrapsDePoche=" + harrapsDePoche.toString());
+
+        System.out.println("Testing Bibliothèque");
+        Bibliotheque maBibliotheque = new Bibliotheque(10);
+        maBibliotheque.ajouter(new Novel(1, "Le rouge et le noir", "Stendhal", 563, null));
+        maBibliotheque.ajouter(
+                new Novel(2, "Leurs enfants après eux", "Nicolas Matthieu", 354, Novel.LiteraryPrice.GONCOURT));
+        maBibliotheque
+                .ajouter(new Manual(7, "Histoire-Géographie-EMC 6e", "Émilie Blanchard", 154, Manual.Level.COLLEGE));
+        maBibliotheque.ajouter(new Magazine(56, "Géo", 6, 2019));
+        maBibliotheque.ajouter(new Dictionary(82, "Harrap's de poche", Dictionary.Language.ENGLISH));
+        maBibliotheque.afficherDocuments();
+        maBibliotheque.afficherAuteurs();
     }
 }
